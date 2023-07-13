288cef4918dfd72dc0b853b990b6fcee68fc9842
Sagar Tamang <sagar.bdr0000@gmail.com>
2023-07-13 08:51:23 +0530

    analysis

--- main.py
+++ main.py
@@ -4,28 +4,18 @@ import numpy as np
 naukri=pd.read_excel('NaukriJobListing_2023-07-11.xlsx')
 
 counter = 1
-unique_title = naukri['Job Title']
+# unique_title = len(naukri['Job Title'].unique())
+# print("Number of unique titles: " + str(unique_title))
 
-print("unique_title")
+duplicate_title = naukri['Job Title'].duplicated().sum()
+print(duplicate_title)
 
+# print(naukri.isnull().any().sum())
+# naukri.fillna(value=None, method=None)
+# print(naukri.isnull().any().sum())
 
 # Number of jobs today
 
 # Number of jobs for graduates today
 
 # Number of jobs for post-graduates today 
-
-chunk_size = 10000
-
-# Initialize an empty set to store unique job titles
-unique_job_titles = set()
-
-# Iterate through the dataset in chunks
-for chunk in pd.read_excel('NaukriJobListing_2023-07-11.xlsx', chunksize=chunk_size):
-    # Extract unique job titles from the current chunk
-    unique_job_titles.update(chunk['Job Title'].unique())
-
-# Calculate the count of unique job titles
-num_unique_job_titles = len(unique_job_titles)
-
-print("The number of unique job titles is:", num_unique_job_titles)
